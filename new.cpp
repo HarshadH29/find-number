4da005d3346fb867eca7676491166d3d9ab1a6fe
HarshadH@10298 <harshadhingane@gmail.com>
2024-02-11 13:48:22 +0530

    Find number

---
+++ new.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+ int main()
+{
+    int a,b,c;
+    printf("Enter the two numbers\n");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("The ans is %d",c);
+    return 0;
+    
+}
